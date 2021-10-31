51b0d9c1b876a5b1d16a5af3554a3deb55c61a68
Tung Pham <tung.tufts@gmail.com>
2021-10-31 19:50:47 -0400

    add

--- pages/careers.js
+++ pages/careers.js
@@ -71,7 +71,7 @@ const careers = ({
 										<h2 className="font-medium text-2xl"> {name} </h2>
 										<p className="mt-2">{description}</p>
 									</div> */}
-									<div>
+									<div class="col-span-12 rounded-lg p-8 bg-white bg-opacity-5">
 										<h2 className="font-medium text-2xl"> {name}</h2>
 										<p className="mt-2">{description}</p>
 									</div>
