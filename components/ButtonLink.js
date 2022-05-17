765cede23252430e9840170564856f881e92d0cb
Anh <ledzanh@gmail.com>
2022-05-17 16:51:40 +0700

    fix: type button style in safari

--- components/ButtonLink.js
+++ components/ButtonLink.js
@@ -4,9 +4,7 @@ const ButtonLink = props => {
   const { text, url } = props;
   return (
     <Link href={url}>
-      <a className="border px-6 py-2 rounded-md" type="button">
-        {text}
-      </a>
+      <a className="border px-6 py-2 rounded-md">{text}</a>
     </Link>
   );
 };
